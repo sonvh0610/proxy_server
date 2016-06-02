15abc49a30c959ae65adf3e07fe8b01917707e53
vhson0610 <vector.mic@gmail.com>
2016-06-02 21:38:29 +0700

    Add files via upload

--- DoAn_Socket/Client.cs
+++ DoAn_Socket/Client.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
